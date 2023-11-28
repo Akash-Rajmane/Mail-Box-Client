55d53f72f2588169e76c40fe1694129d2e279ab2
Akash <akashrajmane23@gmail.com>
2023-11-28 12:25:50 +0530

    added signup page

--- src/index.js
+++ src/index.js
@@ -3,11 +3,20 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "../node_modules/bootstrap-icons/font/bootstrap-icons.min.css";
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from "./store/index";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 );
 
