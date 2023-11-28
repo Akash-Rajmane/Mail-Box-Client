df321174a4c63336e00defd910c4744c886882cb
Akash <akashrajmane23@gmail.com>
2023-11-28 14:35:44 +0530

    added send mail functionality

--- src/components/UI/Notification.js
+++ src/components/UI/Notification.js
@@ -1,7 +1,18 @@
 import { Alert } from "react-bootstrap";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { showNotification } from "../../store/authSlice";
 
 
 const Notification = (props) => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      dispatch(showNotification({ message: null, variant: null }));
+    }, 3000);
+
+    return () => clearTimeout(timer);
+  }, [dispatch]);
  
   return <Alert variant={props.variant} >{props.message}</Alert>;
 };
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import MailboxEditor from '../components/Mailbox/MailboxEditor';
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div>
+        <MailboxEditor/>
+    </div>
   )
 }
 
