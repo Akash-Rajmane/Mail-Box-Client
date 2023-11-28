5e1b6ee27498980f980a57150144e4409a819411
Akash <akashrajmane23@gmail.com>
2023-11-28 15:06:42 +0530

    added get req, inbox page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,40 @@
+import { useEffect } from 'react';
 import './App.css';
 import Auth from './components/Auth';
 import Home from "./pages/Home";
 import { Routes, Route } from 'react-router-dom';
+import { addToInbox } from "./store/mailSlice";
+import axios from "axios";
+import { useDispatch } from 'react-redux';
 
 function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const getEmails = async () => {
+      try {
+        const response = await axios.get(
+          "https://react-mailbox-client-4f470-default-rtdb.firebaseio.com/emails.json",
+          {
+            headers: {
+              "Content-Type": "application/json",
+            },
+          }
+        );
+        const data = response.data;
+        if (response.status === 200) {
+          for (const key in data) {
+            const mailItem = { id: key, isChecked: false, ...data[key] };
+            dispatch(addToInbox(mailItem));
+          }
+        }
+      } catch (e) {
+        console.log(e.message);
+      }
+    };
+    getEmails();
+  }, [dispatch]);
+
   return (
     <div className="App">
       <Routes>
